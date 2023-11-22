93c2ecbc6ea4987a47df8e42a32694df9aceb012
Snizhana Mohyl <snizhana.m@voodoo-digital.com>
2023-11-22 13:07:07 +0200

    add: truncating text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,14 @@
 // API
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 const API_KEY = 'live_ST09boZt58lcEhnCapTTn4JAcUEB72aRfAuLhpU5pxAiAImePNDmuCEvlyaNB56Q';
-
-// https://api.thecatapi.com/v1/images/search?limit=11&page=2&api_key=live_ST09boZt58lcEhnCapTTn4JAcUEB72aRfAuLhpU5pxAiAImePNDmuCEvlyaNB56Q
-
 const limit = 11;
 
-const getAllCats = (page = 1) => {
-  const catsData = [];
+const getAllCats = async (page = 1) => {
   try {
-    fetch(`${BASE_URL}?limit=${limit}&api_key=${API_KEY}`)
-      .then((res) => res.json())
-      .then((data) => {
-        catsData.push(data);
-      });
+    const catsData = [];
+    const res = await fetch(`${BASE_URL}?limit=${limit}&has_breeds=1&api_key=${API_KEY}`);
+    const data = await res.json();
+    catsData.push(...data);
     return catsData;
   } catch (error) {
     console.error('Error occured on fetching products:', error);
@@ -23,10 +18,9 @@ const getAllCats = (page = 1) => {
 
 // MARKUP
 const createCatsListMarkup = (cats) => {
-  const catsData = [...cats];
   const listMarkup = cats.reduce((markup, cat) => {
     const { id, url } = cat;
-    console.log('🚀 ~ file: index.js:27 ~ listMarkup ~ url:', cat);
+    const { name, description } = cat.breeds[0];
 
     // const img = images[0]?.src ? images[0]?.src : 'https://cdn-icons-png.flaticon.com/512/8676/8676496.png';
 
@@ -38,10 +32,8 @@ const createCatsListMarkup = (cats) => {
               <button class="cats__like-btn"></button>
             </div>
             <div class="cats__info-wrapper">
-              <h3 class="cats__title">Lorem ipsum</h3>
-              <p class="cats__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit fermentum a accumsan,
-                consequat vitae arcu velit, nunc,
-                suspendisse.</p>
+              <h3 class="cats__title">${name}</h3>
+              <p class="cats__description">${description}</p>
             </div>
           </li>`;
 
@@ -56,10 +48,10 @@ const createCatsListMarkup = (cats) => {
 const cardListEl = document.querySelector('.cats__list');
 // const errorMsgEl = document.getElementById('error-msg');
 
-const renderList = (page) => {
+const renderList = async (page) => {
   // paginationEl.classList.add('pointer-events-none');
 
-  const cats = getAllCats(page);
+  const cats = await getAllCats(page);
 
   // if (!cats) {
   //   cardListEl.classList.add('hidden');
